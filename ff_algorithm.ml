bfc5f3eb8b9fc759026f21a313d6169297615b7c
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2018-11-16 19:50:00 +0100

    correction of find_path : it work now ! (no stack overflow anymore)

--- ff_algorithm.ml
+++ ff_algorithm.ml
@@ -1,25 +1,21 @@
 open Graph
-open Printf
     
-type path = 'a list
+type path = id list
 
-(* before call find_path : source must be in forbidden *)
-let rec find_path_bis graph forbidden source sink = (*if (sink=source) then forbidden else*)
+let rec find_path graph path source sink = 
+	Printf.printf "noeud %s \n" source;
+	if (sink=source) then (source::path) else
 	let arcs_src = out_arcs graph source in
 	let rec loop arcs_src =	
 		match arcs_src with
 		| [] -> []
-		| (id, _)::rest ->  if id=sink then (id::forbidden) else 
-			let aux = find_path graph (id::forbidden) id sink in
+		| (id, _)::rest -> 
+	if (List.exists (fun x -> (x=id)) path) then loop rest
+	else let aux = find_path graph (source::path) id sink in
 				match aux with
 					| [] -> loop rest 
 					| aux -> aux
 	in loop arcs_src
-	
-let find_path graph forbidden source sink = find_path_bis graph (source::forbidden) source sink
-
 
 (* let Ford_Fulkerson_Algorithm graph source sink *)
-
-
 
