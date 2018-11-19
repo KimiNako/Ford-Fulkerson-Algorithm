d8e9f87bacaf45481167918462a77eb475d9027c
KimiNako <katran@etud.insa-toulouse.fr>
2018-11-19 18:20:05 +0100

    Added some functions about residual graph

--- ff_algorithm.ml
+++ ff_algorithm.ml
@@ -15,17 +15,46 @@ let rec find_path graph path source sink =
 		(* loop with the out arcs of source node *)
 		let rec loop arcs_src =	
 			match arcs_src with
-			| [] -> [] (* no path found *)
-			| (id, _)::rest -> 
-				(* if the node reached is already in the path, the loop is continued *)
-				if (List.exists (fun x -> (x=id)) path) then loop rest
-				(* else a path through this node is searched *)
-				else let aux = find_path graph (source::path) id sink in
-							match aux with
-								| [] -> loop rest (* no path, the research is continued *)
-								| aux -> aux (* a path has been found ! *)
+				| [] -> [] (* no path found *)
+				| (id, _)::rest -> 
+					(* if the node reached is already in the path, the loop is continued *)
+					if (List.exists (fun x -> (x=id)) path) then loop rest
+					(* else a path through this node is searched *)
+					else let aux = find_path graph (source::path) id sink in
+								match aux with
+									| [] -> loop rest (* no path, the research is continued *)
+									| aux -> aux (* a path has been found ! *)
 		in loop arcs_src
 
+let rec find_min_arc graph path acu =
+	match path with
+		| [] -> acu
+		| id2::id1::rest -> 
+			let cost = find_arc graph id1 id2 in
+				match cost with
+					| None -> raise Not_found
+					| Some (_,cost) -> if (cost<acu) then find_min_arc graph (id1::rest) cost 
+									else find_min_arc graph (id1::rest) acu
 
-(* let Ford_Fulkerson_Algorithm graph source sink *)
+let update_residual_graph graph residual_graph path min =
+	let rec loop path new_graph = 
+		match path with
+			| [] -> new_graph
+			| id1::id2::rest -> 
+				let residual_arc = find_arc residual_graph id1 id2 in
+				let graph_arc = find_arc graph id2 id1 in (* a finir !!!!!!!!!*) 
+	in loop path empty_graph
+
+
+(*let rec create_residual_graph graph1 graph2 = *)
+
+
+(* arc label of residual_graph : (capacity, value) *)
+let Ford_Fulkerson_Algorithm graph source sink =
+	let flow_graph = map graph (fun x -> (x,0) in
+	let residual_graph = map graph (fun x -> (x, x)) in
+	let rec loop =
+		let path = find_path residual_graph [] source sink in
+		let min = find_min_arc residual_graph path 1000 in (*max value ?? *)
+		
 
