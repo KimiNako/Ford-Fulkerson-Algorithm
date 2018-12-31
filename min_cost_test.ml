74569f42e200fea392788d63a121bf361e35ca8c
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2018-12-31 09:38:28 +0100

    add second test for min cost algo

--- min_cost_test.ml
+++ min_cost_test.ml
@@ -12,9 +12,20 @@ let () =
   	and outfile = Sys.argv.(2)
 
 	in 	
-	Printf.printf "\n------Test : Min_cost_algorithm------\n";
+
+    (* Test 1 : Max_flow_min_cost_algorithm with graph_Better *)
+	Printf.printf "\n------Test : Max_flow_min_cost_algorithm - graph_Better-----\n";
 	Printf.printf "------Check %s_mc_flow_graph.gv file in Tests/GV_files repository------\n\n" outfile;
-	Printf.printf "------Advice : Do \"make dot_mc_test\" in your terminal to visualize the graph in png format and find it in PNG_files repository------\n\n";
 	min_cost_max_flow_algorithm infile outfile;
     Printf.printf "Expected : Maximum of assignments : 4 \n";
     Printf.printf "Expected : Minimum cost :  5 \n\n";
+
+
+    (* Test 2 : Max_flow_min_cost_algorithm with graph_Better2 *)
+	Printf.printf "\n------Test : Min_cost_algorithm - graph_Better2-----\n";
+	Printf.printf "------Check %s2_mc_flow_graph.gv file in Tests/GV_files repository------\n\n" outfile;
+	min_cost_max_flow_algorithm (infile^"2") (outfile^"2");
+    Printf.printf "Expected : Maximum of assignments : 3 \n";
+    Printf.printf "Expected : Minimum cost :  4 \n\n";
+
+	Printf.printf "------Advice : Do \"make dot_mc_test\" in your terminal to visualize the graphs in png format and find them in PNG_files repository------\n\n";
